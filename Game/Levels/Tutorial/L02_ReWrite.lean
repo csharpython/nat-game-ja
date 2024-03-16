edf8224f3588bc1c645d4a1a7fc2861bbd8655d0
Csharpython <ykt8910@gmail.com>
2024-03-16 22:28:29 +0900

    Tacticの説明を追加

--- Game/Levels/Tutorial/L02_ReWrite.lean
+++ Game/Levels/Tutorial/L02_ReWrite.lean
@@ -5,6 +5,27 @@ Level 2
 
 Title "Eq."
 
+/--
+## 説明
+例えば、`X=Y`という形の仮定や定理`h`があった場合、rw[h]を使用することで、
+ゴール内の**全ての**`X`が`Y`に置き換わります。
+
+## 変種
+`rw[←h]` : `Y`が`X`になります。
+
+`rw[h1,h2]` : `rw[h1]`,`rw[h2]`を順番に実行します。
+
+`rw[h1,h2,h3]` : もちろん3つ以上の置き換えも可能です。
+
+`rw[h1] at h2` : **`h2`に対して**`rw[h1]`を実行します。
+
+もしかしたら、私が把握できていないだけで他にもあるかも知れません！
+
+## 補足情報
+本来の`rw`は置き換え後に`rfl`を実行します。
+そうしなかった理由は、もちろんゲーム性です。
+-/
+TacticDoc rw
 Introduction "同じことを表すものってよくありますよね。
 例えば、「力」と「パワー」や、「1の次の数」と「2」などがあります。
 Lean4で同じことを表すものを置き換えるときは\"rewrite\"を使います。
@@ -18,12 +39,12 @@ Statement (e mc:Nat)(h : e = mc ^ 2) : e = mc ^ 2 := by
 
 Conclusion "いいですね！Lean4では、仮定として用意した等式だけでなく、
 すでに分かったこと、「定理」(もしくは「補題」)を番う方法もあります！
-詳細は次のステージで！
+詳細は次のステージで！\n
 ちなみに、`rewrite`より強いTactic`rw`がありますが、
 それを使わなかった理由はもちろん、ゲーム性を上げるためです。"
 --rewriteを使えるようにするとrwも使えるようになるそうです。現在修正中
 /- Use these commands to add items to the game's inventory. -/
 
-NewTactic rewrite
+NewTactic rw
 -- NewLemma Nat.add_comm Nat.add_assoc
 -- NewDefinition Nat Add Eq
