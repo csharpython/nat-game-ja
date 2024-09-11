7a6e4058a1a47dd201352ef186478273d2d729dc
Csharpython <ykt8910@gmail.com>
2024-09-11 22:31:58 +0900

    完成：MulEX
    Negaの作成に着手します

--- Game/Levels/MulEX.lean
+++ Game/Levels/MulEX.lean
@@ -6,6 +6,7 @@ import Game.Levels.MulEX.L05_mul_left_ne_zero
 import Game.Levels.MulEX.L06_intro_forall
 import Game.Levels.MulEX.L07_mul_ne_zero
 import Game.Levels.MulEX.L08_mul_right_cancel
+import Game.Levels.MulEX.L09_mul_right_eq_one
 
 World "MulEX"
 Title "World x EX"
